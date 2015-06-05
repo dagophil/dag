bf3ac4c7a8fb630476eecaf8889410fdb268dbd1
Philip Schill <philip.schill@gmx.de>
2015-06-05 11:38:29 +0200

    The TwoClassSVM is working now :) But it still misses good stopping criteria. Added SVM test with toy data.

--- include/vigra/svm.hxx
+++ include/vigra/svm.hxx
@@ -15,6 +15,32 @@ namespace vigra
 
 
 
+//namespace detail
+//{
+//    /// \brief Compute the dot product of the given instances.
+//    template <typename FEATURES>
+//    class DotProductEvaluater
+//    {
+//    public:
+//        typedef FEATURES Features;
+//        DotProductEvaluater(Features const & features)
+//            : features_(features)
+//        {}
+//        double operator()(size_t i, size_t j) const
+//        {
+//            double v = 0;
+//            for (size_t k = 0; k < features_.shape()[1]; ++k)
+//            {
+//                v += features_(i, k) * features_(j, k);
+//            }
+//        }
+//    protected:
+//        Features const & features_;
+//    };
+//}
+
+
+
 /// \brief Two class support vector machine using hinge loss and a quadratic regularizer (implemented with "dual coordinate descent" [Hsieh et al. 2008])
 template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE = MersenneTwister>
 class TwoClassSVM
@@ -51,33 +77,25 @@ public:
 
 protected:
 
+    /// \brief Find mean and standard deviation of the given features and save them.
+    template <typename FEATURES>
+    void find_normalization(
+            FEATURES const & features
+    );
+
+    /// \brief Apply the current normalization on the given features and add the bias feature.
+    template <typename FEATURES>
+    void apply_normalization(
+            FEATURES const & features_in,
+            MultiArray<2, double> & features_out
+    ) const;
+
     RandEngine const & randengine_;
     std::vector<LabelType> distinct_labels_;
     MultiArray<1, double> beta_;
     MultiArray<1, double> mean_;
     MultiArray<1, double> std_dev_;
-};
-
-
-
-template <typename FEATURES>
-class DotProductEvaluater
-{
-public:
-    typedef FEATURES Features;
-    DotProductEvaluater(Features const & features)
-        : features_(features)
-    {}
-    double operator()(size_t i, size_t j) const
-    {
-        double v = 0;
-        for (size_t k = 0; k < features_.shape()[1]; ++k)
-        {
-            v += features_(i, k) * features_(j, k);
-        }
-    }
-protected:
-    Features const & features_;
+    double B_; // value of the bias feature
 };
 
 template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE>
@@ -94,8 +112,9 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::train(
 
     double const U = 1.0; // TODO: Make this a parameter.
     size_t const num_instances = features.shape()[0];
-    size_t const num_features = features.shape()[1];
+    size_t num_features = features.shape()[1];
     double const tol = 0.0001;
+    B_ = 1.5; // value of the bias feature
 
     // Find the distinct labels.
     auto dlabels = std::set<LabelType>(labels.begin(), labels.end());
@@ -107,42 +126,11 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::train(
     auto label_ids = MultiArray<1, int>(labels.size());
     transform_external_labels(labels, label_ids);
 
-    // Normalize the data.
-    mean_.reshape(Shape1(num_features), 0.);
-    for (size_t j = 0; j < num_features; ++j)
-    {
-        for (size_t i = 0; i < num_instances; ++i)
-        {
-            mean_[j] += features(i, j);
-        }
-        mean_[j] /= num_instances;
-    }
-    std_dev_.reshape(Shape1(num_features), 0.);
-    for (size_t j = 0; j < num_features; ++j)
-    {
-        for (size_t i = 0; i < num_instances; ++i)
-        {
-            double v = features(i, j) - mean_[j];
-            std_dev_[j] += v*v;
-        }
-        std_dev_[j] = std::sqrt(std_dev_[j] / (num_instances-1.));
-        if (std_dev_[j] == 0)
-        {
-            std::cout << "Warning: Standard deviation is zero, division by zero will occur." << std::endl;
-        }
-    }
+    // Normalize the features.
+    ++num_features; // increase the number since the bias feature will be added
     auto normalized_features = MultiArray<2, double>(Shape2(num_instances, num_features));
-    for (size_t j = 0; j < num_features; ++j)
-    {
-        for (size_t i = 0; i < num_instances; ++i)
-        {
-            normalized_features(i, j) = (features(i, j) - mean_[j]) / std_dev_[j];
-        }
-    }
-
-    // Initialize the alphas and betas with 0.
-    auto alpha = MultiArray<1, double>(Shape1(num_instances), 0.);
-    beta_.reshape(Shape1(num_features), 0.);
+    find_normalization(features);
+    apply_normalization(features, normalized_features);
 
     // Precompute the squared norm of the instances.
     auto x_squ = MultiArray<1, double>(Shape1(num_instances));
@@ -157,7 +145,9 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::train(
         x_squ(i) = v;
     }
 
-    // Do the SVM loop.
+    // Initialize the alphas and betas with 0 and do the SVM loop.
+    auto alpha = MultiArray<1, double>(Shape1(num_instances), 0.);
+    beta_.reshape(Shape1(num_features), 0.);
     auto indices = std::vector<size_t>(num_instances);
     std::iota(indices.begin(), indices.end(), 0);
     auto rand_int = UniformIntRandomFunctor<RandEngine>(randengine_);
@@ -185,17 +175,12 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::train(
                 ++diff_count;
                 for (size_t j = 0; j < num_features; ++j)
                 {
-                    beta_(j) += label_ids(j) * normalized_features(i, j) * (alpha(i) - old_alpha);
+                    beta_(j) += label_ids(i) * normalized_features(i, j) * (alpha(i) - old_alpha);
                 }
             }
 
 
-
             ++t;
-            if (t % 500000 == 0)
-            {
-                std::cout << t << std::endl;
-            }
             if (t >= max_t)
             {
                 break;
@@ -203,32 +188,12 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::train(
         }
 
         std::cout << "diffs: " << diff_count << std::endl;
-        if (1000*diff_count < num_instances)
+//        if (1000*diff_count < num_instances)
+        if (diff_count < 1000)
         {
             break;
         }
     }
-
-    size_t c0 = 0;
-    size_t c1 = 0;
-    for (size_t i = 0; i < alpha.size(); ++i)
-    {
-        if (alpha(i) == 0)
-            ++c0;
-        if (alpha(i) == U)
-            ++c1;
-    }
-    std::cout << c0 << " of " << alpha.size() << " values == 0" << std::endl;
-    std::cout << c1 << " of " << alpha.size() << " values == C" << std::endl;
-    std::cout << (c0+c1) << " of " << alpha.size() << " values are 0 or C" << std::endl;
-
-//    std::cout << "alpha: ";
-//    for (size_t i = 0; i < alpha.size(); ++i) std::cout << alpha(i) << ", ";
-//    std::cout << std::endl;
-//    std::cout << "beta: ";
-//    for (size_t i = 0; i < beta_.size(); ++i) std::cout << beta_(i) << ", ";
-//    std::cout << std::endl;
-//    std::cout << "classes: " << (int)distinct_labels_[0] << ": +1, " << (int)distinct_labels_[1] << ": -1" << std::endl;
 }
 
 template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE>
@@ -243,25 +208,26 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::predict(
                   "TwoClassSVM::predict(): Wrong label type.");
     vigra_precondition(distinct_labels_.size() == 2,
                        "TwoClassSVM::predict(): Number of labels found in training must be 2.");
-    vigra_precondition(features.shape()[1] == beta_.size(),
+    vigra_precondition(features.shape()[1]+1 == beta_.size(),
                        "TwoClassSVM::predict(): Wrong number of features.");
 
     size_t const num_instances = features.shape()[0];
-    size_t const num_features = features.shape()[1];
+    size_t const num_features = features.shape()[1]+1;
+
+    auto normalized_features = MultiArray<2, double>(Shape2(num_instances, num_features));
+    apply_normalization(features, normalized_features);
 
     for (size_t i = 0; i < num_instances; ++i)
     {
         double v = 0;
         for (size_t j = 0; j < num_features; ++j)
         {
-            v += (features(i, j) - mean_[j]) / std_dev_[j] * beta_(j);
+            v += normalized_features(i, j) * beta_(j);
         }
         size_t index = (v >= 0) ? 0 : 1; // if v >= 0 then we use label +1, which has index 0 in distinct_labels_, else we use the label with index 1
         labels(i) = distinct_labels_[index];
     }
 }
-
-
 
 template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE>
 template <typename LABELS>
@@ -283,6 +249,74 @@ void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::transform_external_labels(
     for (size_t i = 0; i < labels_in.size(); ++i)
     {
         labels_out(i) = label_ids[labels_in(i)];
+    }
+}
+
+template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE>
+template <typename FEATURES>
+void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::find_normalization(
+        FEATURES const & features
+){
+    static_assert(std::is_convertible<typename FEATURES::value_type, FeatureType>(),
+                  "TwoClassSVM::find_normalization(): Wrong feature type.");
+
+    size_t const num_instances = features.shape()[0];
+    size_t const num_features = features.shape()[1];
+
+    // Find the mean.
+    mean_.reshape(Shape1(num_features), 0.);
+    for (size_t j = 0; j < num_features; ++j)
+    {
+        for (size_t i = 0; i < num_instances; ++i)
+        {
+            mean_[j] += features(i, j);
+        }
+        mean_[j] /= num_instances;
+    }
+
+    // Find the standard deviation.
+    std_dev_.reshape(Shape1(num_features), 0.);
+    for (size_t j = 0; j < num_features; ++j)
+    {
+        for (size_t i = 0; i < num_instances; ++i)
+        {
+            double v = features(i, j) - mean_[j];
+            std_dev_[j] += v*v;
+        }
+        std_dev_[j] = std::sqrt(std_dev_[j] / (num_instances-1.));
+        if (std_dev_[j] == 0)
+        {
+            std::cout << "Warning: Standard deviation is zero, so a division by zero will occur." << std::endl;
+        }
+    }
+}
+
+template <typename FEATURETYPE, typename LABELTYPE, typename RANDENGINE>
+template <typename FEATURES>
+void TwoClassSVM<FEATURETYPE, LABELTYPE, RANDENGINE>::apply_normalization(
+        FEATURES const & features_in,
+        MultiArray<2, double> & features_out
+) const {
+    static_assert(std::is_convertible<typename FEATURES::value_type, FeatureType>(),
+                  "TwoClassSVM::normalize(): Wrong feature type.");
+
+    size_t const num_instances = features_in.shape()[0];
+    size_t const num_features = features_in.shape()[1];
+
+    // Normalize the data.
+    features_out.reshape(Shape2(num_instances, num_features+1));
+    for (size_t j = 0; j < num_features; ++j)
+    {
+        for (size_t i = 0; i < num_instances; ++i)
+        {
+            features_out(i, j) = (features_in(i, j) - mean_[j]) / std_dev_[j];
+        }
+    }
+
+    // Add the bias feature.
+    for (size_t i = 0; i < num_instances; ++i)
+    {
+        features_out(i, num_features) = B_;
     }
 }
 
--- test/svm_test.cxx
+++ test/svm_test.cxx
@@ -10,61 +10,69 @@ void test_svm()
     std::cout << "called test_svm()" << std::endl;
 
     {
+        std::cout << "SVM on toy data" << std::endl;
+
         typedef double FeatureType;
         typedef UInt8 LabelType;
 
         std::vector<FeatureType> train_data_x {
-            9,  9, 11,  12, 12,
+            9,  9, 10.5,  12, 12,
             2, -1,  0.5, 2, -1
         };
-        MultiArray<2, FeatureType> train_x(Shape2(5, 2));
-        for (size_t i = 0; i < train_data_x.size(); ++i)
-            train_x[i] = train_data_x[i];
-
         std::vector<LabelType> train_data_y {
             0, 0, 0, 1, 1
         };
-        MultiArray<1, LabelType> train_y(train_data_y.size());
-        for (size_t i = 0; i < train_data_y.size(); ++i)
-            train_y[i] = train_data_y[i];
-
-        TwoClassSVM<FeatureType, LabelType> svm;
-        svm.train(train_x, train_y);
-
-
         std::vector<FeatureType> test_data_x {
             0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
             0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0
         };
+        std::vector<LabelType> test_data_y {
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1
+        };
+
+        // Transform the data into multi arrays.
+        MultiArray<2, FeatureType> train_x(Shape2(5, 2));
+        for (size_t i = 0; i < train_data_x.size(); ++i)
+            train_x[i] = train_data_x[i];
+
+        MultiArray<1, LabelType> train_y(train_data_y.size());
+        for (size_t i = 0; i < train_data_y.size(); ++i)
+            train_y[i] = train_data_y[i];
+
         MultiArray<2, FeatureType> test_x(Shape2(15, 2));
         for (size_t i = 0; i < test_data_x.size(); ++i)
             test_x[i] = test_data_x[i];
 
+        MultiArray<1, LabelType> test_y(test_data_y.size());
+        for (size_t i = 0; i < test_data_y.size(); ++i)
+            test_y[i] = test_data_y[i];
+
+        // Train and predict with the SVM.
+        TwoClassSVM<FeatureType, LabelType> svm;
+        svm.train(train_x, train_y);
         MultiArray<1, LabelType> pred_y(15);
         svm.predict(test_x, pred_y);
 
-        std::cout << "pred: ";
-        for (size_t i = 0; i < pred_y.size(); ++i)
-            std::cout << (int) pred_y(i) << ", ";
-        std::cout << std::endl;
-
-
-
+        // Count the correct predicted instances.
+        size_t count = 0;
+        for (size_t i = 0; i < test_y.size(); ++i)
+        {
+            if (pred_y(i) == test_y(i))
+                ++count;
+        }
+        std::cout << "Performance: " << (count / ((float) pred_y.size())) << " (" << count << " of " << pred_y.size() << ")" << std::endl;
     }
 
-
-
     {
+        std::cout << "SVM on MNIST 3 vs 8" << std::endl;
+
         typedef double FeatureType;
         typedef UInt8 LabelType;
 
         // Load the data.
         std::string train_filename = "/home/philip/data/ml-koethe/train.h5";
         std::string test_filename = "/home/philip/data/ml-koethe/test.h5";
-        std::vector<LabelType> labels = {0, 1};
-//        std::string train_filename = "/home/philip/data/liblinear/susy_float64_train.h5";
-//        std::string test_filename = "/home/philip/data/liblinear/susy_float64_test.h5";
-//        std::vector<LabelType> labels = {0, 1};
+        std::vector<LabelType> labels = {5, 8};
         MultiArray<2, FeatureType> train_x;
         MultiArray<1, LabelType> train_y;
         MultiArray<2, FeatureType> test_x;
@@ -87,10 +95,7 @@ void test_svm()
                 ++count;
         }
         std::cout << "Performance: " << (count / ((float) pred_y.size())) << " (" << count << " of " << pred_y.size() << ")" << std::endl;
-
     }
-
-
 
     std::cout << "finished test_svm()" << std::endl;
 }
